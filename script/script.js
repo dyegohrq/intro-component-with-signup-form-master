ead5a8925f8a8da21fd19553be435824065cc16a
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-05-29 08:57:52 -0300

    adicionando o JavaScript

--- script/script.js
+++ script/script.js
@@ -7,19 +7,41 @@ const password = document.getElementById('Password')
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    checkInpus();
-})
+    checkInputs();
+});
 
-function checkInpus() {
+function checkInputs() {
     const firstNameValue = firstName.value;
     const lastNameValue = lastName.value;
     const emailValue = email.value;
-    const password = password.value; 
+    const passwordValue = password.value; 
 
     if (firstNameValue === '') {
         setErrorFor(firstName, 'First Name cannot be emoty');
     } else {
         setSuccessFor(firstName);
+    }
+
+    if (lastNameValue === '') {
+        setErrorFor(lastName, 'Last Name cannot be empty');
+    } else {
+        setSuccessFor(lastName);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email,'Looks like this is not an email');
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, 'Please enter a valid email.');
+    } else{
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be empty');
+    } else if (passwordValue.length < 7) {
+        setErrorFor(password, 'Password must be at least 7 characters long');
+    } else{
+        setSuccessFor(password);
     }
 
 
@@ -46,4 +68,10 @@ function setSuccessFor(input) {
 
     // Adicionar a class de sucesso 
     formControl.className = 'form-control success';
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
 }
